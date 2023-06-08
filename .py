05183c15937accbd7f88b0c4f22a41f00291dd6a
Gabriel   A.  Teixeira <132387258+GabrielVesal@users.noreply.github.com>
2023-06-08 15:33:01 -0300

    .py

---
+++ .py
@@ -0,0 +1,19 @@
+condutividade = float(input("Informe a condutividade termica do isolamento (W/(m.K)): "))
+
+temperaturadomeiorefrigerado = float(input("Informe a temperatura do meio refrigerado (°C): "))
+
+temperaturadebulboseco = float(input("Informe a temperatura de bulbo seco (°C): "))
+
+umidaderelativa = float(input("Informe a umidade relativa (%%): "))
+
+conveccaexterna = float(input("Informe o coeficiente de convecção externo (W/(m².°C)): "))
+
+conveccaointerna = float(input("Informe o coeficiente de convecção interno (W/(m².°C)): "))
+
+temperaturadeorvalho = (109.8 + temperaturadebulboseco) * pow((umidaderelativa / 100), 0.1247) - 109.8;
+espressuranecessariadoisolante = condutividade * ((temperaturadeorvalho - temperaturadomeiorefrigerado) / (temperaturadebulboseco - temperaturadeorvalho) * 1 /conveccaointerna - 1/conveccaexterna);
+
+print("Temperatura do orvalho: ", temperaturadeorvalho)
+print("Espessura do isolamento: ", espressuranecessariadoisolante * 1000)
+
+  
